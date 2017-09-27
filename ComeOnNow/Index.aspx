af53ab7e5c572eaff1205b28236d598893be5237
dornbob <dornblnr@dukes.jmu.edu>
2017-09-27 00:19:37 -0400

    First Commit

---
+++ ComeOnNow/Index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Home Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            First Name <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Last Name <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnSubmit" Text="Submit" runat="server" OnClick="btnSubmit_Click" /> 
+            <label id="lblInsertSuccess" runat="server">Successfully Inserted!</label>
+            <br />
+            <asp:Button ID="btnTruncate" Text="Truncate Table" runat="server" OnClick="btnTruncate_Click" />
+            <label id="lblTruncateSuccess" runat="server">Table Successfully Truncated!</label>
+        </div>
+    </form>
+    
+</body>
+</html>
---
+++ ComeOnNow/Index.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+
+public partial class Index : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblInsertSuccess.Visible = false;
+        lblTruncateSuccess.Visible = false;
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["con"].ConnectionString))
+        {
+            String sqlQuery = "INSERT INTO person (FirstName,LastName) VALUES (@FirstName,@LastName)";
+
+            using (SqlCommand command = new SqlCommand(sqlQuery, con))
+            {
+                command.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                command.Parameters.AddWithValue("@LastName", txtLastName.Text);
+
+                con.Open();
+                int result = command.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    lblInsertSuccess.Visible = true;
+                    txtFirstName.Text = "";
+                    txtLastName.Text = "";
+                }
+            }
+        }
+    }
+
+
+
+    protected void btnTruncate_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["con"].ConnectionString))
+        {
+            String sqlQuery = "TRUNCATE TABLE person";
+
+            using (SqlCommand command = new SqlCommand(sqlQuery, con))
+            {
+                con.Open();
+                int result = command.ExecuteNonQuery();
+
+                if (result < 0)
+                {
+                    lblTruncateSuccess.Visible = true;
+                }
+            }
+        }
+    }
+}
